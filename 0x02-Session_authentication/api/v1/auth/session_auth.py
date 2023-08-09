c00bdc1557d8f5d0ba6a18ab66c6b07cfab5c73b
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-09 22:18:57 +0000

    added a file

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+"""Session Auth"""
+from api.v1.auth.auth import Auth
+from typing import Tuple, TypeVar
+from models.user import User
+
+
+class SessionAuth(Auth):
+    """ Session Auth
+    """
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ Create Session
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        from uuid import uuid4
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ User ID for Session ID
+        """
+        if session_id is None or type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ Current User
+        """
+        session_id = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_id)
+        return User.get(user_id)
+
+    def destroy_session(self, request=None):
+        """Destroy a session"""
+        if request is None:
+            return False
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return False
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_id]
+        return True
+
