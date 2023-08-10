fd26d3e01d781c6fc0c874ac719c96bbecc231a0
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-10 19:53:29 +0000

    updated a file

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,6 +4,12 @@
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
+from os import getenv
+
+
+if getenv("AUTH_TYPE") == 'session_auth':
+    from api.v1.auth.session_auth import SessionAuth
+    auth = SessionAuth()
 
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
@@ -25,13 +31,14 @@ def view_one_user(user_id: str = None) -> str:
       - User object JSON represented
       - 404 if the User ID doesn't exist
     """
-    if user_id == "me" and request.current_user is None:
-        print("Hello")
-        abort(404)
-    if user_id == "me" and request.current_user is not None:
-        return jsonify(request.current_user.to_json())    
     if user_id is None:
         abort(404)
+
+    if user_id == 'me':
+        if not request.current_user:
+            abort(404)
+        return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
