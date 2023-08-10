1c30d2c8621e7fc1c63a26cc7d93310c6d7d275f
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-10 22:04:45 +0000

    added a file

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -5,12 +5,12 @@ from models.base import Base
 
 
 class UserSession(Base):
-        """ UserSession class
-            """
+    """ UserSession class
+    """
 
-                def __init__(self, *args: list, **kwargs: dict):
-                            """ Initialize a User instance
-                                    """
-                                            super().__init__(*args, **kwargs)
-                                                    self.user_id = kwargs.get('user_id')
-                                                            self.session_id = kwargs.get('session_id')
+    def __init__(self, *args: list, **kwargs: dict):
+        """ Initialize a User instance
+        """
+        super().__init__(*args, **kwargs)
+        self.user_id = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
