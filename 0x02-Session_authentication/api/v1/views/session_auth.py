5947333668b56120d296b3ec489578df78921628
Goldjay94 <genevieveezeh786@gmail.com>
2023-08-11 21:05:07 +0000

    updated a file

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -1,42 +1,61 @@
 #!/usr/bin/env python3
+""" Module of Session Auth
 """
-API authentication module
-"""
-
-from flask import request
-from typing import List, TypeVar
+from api.v1.views import app_views
+from flask import abort, jsonify, request
+from models.user import User
 from os import getenv
 
 
-class Auth:
-    """ Authentication """
+@app_views.route('/auth_session/logout',
+                 methods=['DELETE'],
+                 strict_slashes=False)
+def session_logout() -> str:
+    """ DELETE /api/v1/auth_session/logout
+    JSON body:
+    - session id
+    Return:
+      - Empty JSON
+    """
+    from api.v1.app import auth
 
-    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """ Checks if API routes require authentication """
-        if path is None or not excluded_paths:
-            return True
-        for i in excluded_paths:
-            if i.endswith('*') and path.startswith(i[:-1]):
-                return False
-            elif i in {path, path + '/'}:
-                return False
-        return True
+    logout = auth.destroy_session(request)
+    if not logout:
+        abort(404)
+    return jsonify({}), 200
 
-    def authorization_header(self, request=None) -> str:
-        """ Checks if Authorization request header is present
-        & contains values """
-        if request is None or "Authorization" not in request.headers:
-            return None
-        else:
-            return request.headers.get('Authorization')
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """ placeholder """
-        return None
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def session_login() -> str:
+    """ POST /api/v1/auth_session/login
+    JSON body:
+      - email
+      - password
+    Return:
+      - User object JSON represented
+    """
 
-    def session_cookie(self, request=None):
-        """ Returns cookie value from a request """
-        if request is None:
-            return None
+    user_email = request.form.get('email')
+    user_pswd = request.form.get('password')
 
-        return request.cookies.get(getenv('SESSION_NAME'))
+    if not user_email:
+        return jsonify({"error": "email missing"}), 400
+    if not user_pswd:
+        return jsonify({"error": "password missing"}), 400
+
+    try:
+        search_users = User.search({'email': user_email})
+    except Exception:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not search_users:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    user = search_users[0]
+    if not user.is_valid_password(user_pswd):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_cookie = getenv("SESSION_NAME")
+    session_id = auth.create_session(user.id)
+    response = jsonify(user.to_json())
+    response.set_cookie(session_cookie, session_id)
+    return response
